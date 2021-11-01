e42877d4ceee3965dd443fa27dcfb9216939aaab
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-01 19:15:22 +0100

    Add scss variables

--- eat-the-world/src/Home/Home.js
+++ eat-the-world/src/Home/Home.js
@@ -1,9 +1,11 @@
 import Search from './Search';
+import './Home.scss';
 
 const Home = () => {
   return (
     <section className="home">
       <Search />
+      <div>bla</div>
     </section>
   );
 };

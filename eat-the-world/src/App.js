d4305ee1e282d9d9545cdf7e588c9ccea9820fe6
Adriana Ortiz Tosone <adryisa26@gmail.com>
2021-11-01 15:01:05 +0100

    update app made by conflicts on the header

--- eat-the-world/src/App.js
+++ eat-the-world/src/App.js
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
-import Header from "./common/Header/Header";
-=======
+import Header from './common/Header/Header';
 import Home from './Home/Home';
 import './App.scss';
->>>>>>> main
 
 function App() {
   return (
     <main className="main">
-<<<<<<< HEAD
       <Header />
-=======
       <Home />
->>>>>>> main
     </main>
   );
 }

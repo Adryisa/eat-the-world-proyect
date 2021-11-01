059ffb7ad2645383bd418df92702bcd1c9201692
Adriana Ortiz Tosone <adryisa26@gmail.com>
2021-11-01 17:47:33 +0100

    add listPage component with inicial text

---
+++ eat-the-world/src/ListPage/ListPage.js
@@ -0,0 +1,9 @@
+const ListPage = () => {
+  return (
+    <div className="text-container">
+      <p className="text-container__text">Search results for: Rice</p>
+    </div>
+  );
+};
+
+export default ListPage;

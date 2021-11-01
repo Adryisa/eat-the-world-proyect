4d284e9890bd2782ed04fe5bf986c8eab8105f86
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-01 16:31:51 +0100

    Refactor Home component

---
+++ eat-the-world/src/Home/Search.js
@@ -0,0 +1,32 @@
+import './Search.scss';
+import backgroundImage from '../assets/img/fruits-background.jpg';
+
+const Search = () => {
+  return (
+    <section className="search-container">
+      <h2 className="search-container__header">
+        What do you want to eat today?
+      </h2>
+      <img
+        className="search-container__background"
+        src={backgroundImage}
+        alt="A variety of fruits and vegetables"
+      />
+      <form className="search-container-form">
+        <label
+          className="search-container-form__label"
+          htmlFor="search-container-form__input"
+        >
+          Search
+        </label>
+        <input
+          className="search-container-form__input"
+          id="search-container-form__input"
+        />
+        <button className="search-container-form__button">X</button>
+      </form>
+    </section>
+  );
+};
+
+export default Search;

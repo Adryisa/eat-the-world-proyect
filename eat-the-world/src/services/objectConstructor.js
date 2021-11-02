2db46c3b3332b6d6ad650e418f9fcd3a9dd02e30
amorosAlvaro <91912828+amorosAlvaro@users.noreply.github.com>
2021-11-02 12:29:41 +0100

    add API abd object constructor to clean data

---
+++ eat-the-world/src/services/objectConstructor.js
@@ -0,0 +1,23 @@
+// Generate object with cleaned data
+export default function generateObjetRecipe(item) {
+  const recipe = {
+    id: item.idMeal,
+    name: item.strMeal,
+    country: item.strArea,
+    instructions: item.strInstructions,
+    category: item.strCategory,
+    picture: item.strMealThumb,
+    tags: item.strTags,
+    ingredients: [],
+  };
+  for (let i = 1; i < 20; i++) {
+    // Join ingredients and quantities in a single object (filter out empty strings)
+    if (item[`strIngredient${i}`] !== '') {
+      recipe.ingredients.push({
+        ingredient: item[`strIngredient${i}`],
+        quantity: item[`strMeasure${i}`],
+      });
+    }
+  }
+  return recipe;
+}

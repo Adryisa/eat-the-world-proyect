c860c88cfc6dbd4c9745de427e7861229c122a02
amorosAlvaro <91912828+amorosAlvaro@users.noreply.github.com>
2021-11-02 19:15:06 +0100

    add services to details

--- eat-the-world/src/Details/details-page.js
+++ eat-the-world/src/Details/details-page.js
@@ -3,16 +3,24 @@ import sopa from 'assets/img/sopa.jpg';
 import starIcon from 'assets/icons/star.svg';
 import shareIcon from 'assets/icons/share.svg';
 import printIcon from 'assets/icons/cil_print.svg';
+import { useParams } from 'react-router-dom';
+import { useApiContext } from 'contexts/ApiContext';
+import Ingredients from './Ingredients';
 
 export default function DetailsPage({ props }) {
+  let { id } = useParams();
+  const { list } = useApiContext();
+  const recipe = list.find((item) => item.id === id);
+  console.log(recipe);
+
   return (
     <section className="details">
       <div className="details-header">
-        <h2 className="details-header__title">Recipes tittle</h2>
+        <h2 className="details-header__title">{recipe.name}</h2>
         <img
           className="details-picture"
-          src={sopa}
-          alt="Aqui una foto de lado a lado"
+          src={recipe.picture}
+          alt={recipe.name}
         />
       </div>
       <ul className="details-menu">
@@ -27,28 +35,15 @@ export default function DetailsPage({ props }) {
         </li>
       </ul>
       <div className="details-body">
-        <div className="details-details">
-          <h3 className="details-details__title">Details</h3>
-          <div className="details-details-list">
-            <p className="details-details-list__item">4 persons</p>
-            <p className="details-details-list__item">30 min</p>
-          </div>
-        </div>
         <div className="details-ingredients">
           <h3 className="details-ingredients__title">Ingredients</h3>
           <div className="details-ingredients-list">
-            <p className="details-ingredients-list__item">Tomatoes</p>
-            <p className="details-ingredients-list__item">Patatas</p>
+            <Ingredients ingredients={recipe.ingredients} />
           </div>
         </div>
         <div className="details-recipe">
           <h3 className="details-recipe__title">Recipe</h3>
-          <p className="details-recipe__text">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis
-            odio vel quaerat error officia incidunt veritatis, sit praesentium,
-            nisi harum aliquid iste rerum eum, rem corporis cumque molestias
-            fugiat voluptate!
-          </p>
+          <p className="details-recipe__text">{recipe.instructions}</p>
         </div>
       </div>
     </section>

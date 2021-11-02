6b9d54ecb22cc1d9b3bb31508e34c1e469286c92
Adriana Ortiz Tosone <adryisa26@gmail.com>
2021-11-02 18:43:15 +0100

    Add contries on the slider, and useEffect on the apiContect

--- eat-the-world/src/Home/Categories.js
+++ eat-the-world/src/Home/Categories.js
@@ -1,32 +1,29 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useApiContext } from 'contexts/ApiContext';
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
 import './Categories.scss';
 
 const Categories = () => {
+  const { countries } = useApiContext();
+
   return (
     <section className="categories">
       <h2 className="categories__title">Search by category</h2>
       <div className="slider">
         <Swiper spaceBetween={50} slidesPerView={3}>
-          <SwiperSlide>
-            <div className="slider__slide">Test</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slider__slide">Test</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slider__slide">Test</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slider__slide">Test</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slider__slide">Test</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="slider__slide">Test</div>
-          </SwiperSlide>
+          {countries.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className="slider__slide">
+                <img
+                  src={item.image}
+                  className="slider__img"
+                  alt={`${item.country} meals`}
+                />
+                <p className="slider__text">{item.country}</p>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </section>

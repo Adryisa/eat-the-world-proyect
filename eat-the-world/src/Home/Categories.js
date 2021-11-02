bce7f60f4529aae8bd4398f6b786dea6664c53dc
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-02 10:34:00 +0100

    Add preliminary categories section

--- eat-the-world/src/Home/Categories.js
+++ eat-the-world/src/Home/Categories.js
@@ -1,20 +1,31 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import React from 'react';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import './Categories.scss';
 
 const Categories = () => {
   return (
     <section className="categories">
-      {/* <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-      </Swiper> */}
+      <Swiper spaceBetween={50} slidesPerView={3}>
+        <SwiperSlide>
+          <div className="slider__slide">Test</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slider__slide">Test</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slider__slide">Test</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slider__slide">Test</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slider__slide">Test</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slider__slide">Test</div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };
--- eat-the-world/src/Home/Home.js
+++ eat-the-world/src/Home/Home.js
@@ -1,10 +1,12 @@
 import Search from './Search';
+import Categories from './Categories';
 import './Home.scss';
 
 const Home = () => {
   return (
     <section className="home">
       <Search />
+      <Categories />
     </section>
   );
 };

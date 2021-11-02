f223f92853bc844cca08e06b33f22fe79fda87f9
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-02 22:19:12 +0100

    Add preliminary no results found logic

--- eat-the-world/src/ListPage/ListPage.js
+++ eat-the-world/src/ListPage/ListPage.js
@@ -1,12 +1,18 @@
 import ListRecipe from 'common/ListRecipe/ListRecipe';
 import { useApiContext } from 'contexts/ApiContext';
+import noResultsImg from 'assets/img/crying-orange.png';
 
 const ListPage = () => {
   const { list } = useApiContext();
+  console.log(list);
   return (
     <section className="list-page">
       <p className="list-page__text">Search results for: Rice</p>
-      <ListRecipe list={list} />
+      {list ? (
+        <ListRecipe list={list} />
+      ) : (
+        <img src={noResultsImg} alt="Orange emoji no results found" />
+      )}
     </section>
   );
 };

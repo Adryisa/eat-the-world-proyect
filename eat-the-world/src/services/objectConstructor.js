59b1b5c849212da3c50624a6a18551e88bf4615c
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-03 00:45:37 +0100

    Optimize ApiService

--- eat-the-world/src/services/objectConstructor.js
+++ eat-the-world/src/services/objectConstructor.js
@@ -1,9 +1,11 @@
 // Generate object with cleaned data
 export default function generateObjetRecipe(item) {
+  const mealCountry =
+    item.strArea === 'Unknown' ? 'Other countries' : item.strArea;
   const recipe = {
     id: item.idMeal,
     name: item.strMeal,
-    country: item.strArea,
+    country: mealCountry,
     instructions: item.strInstructions,
     category: item.strCategory,
     picture: item.strMealThumb,
@@ -12,7 +14,7 @@ export default function generateObjetRecipe(item) {
   };
   for (let i = 1; i < 20; i++) {
     // Join ingredients and quantities in a single object (filter out empty strings)
-    if (item[`strIngredient${i}`] !== '') {
+    if (item[`strIngredient${i}`]) {
       recipe.ingredients.push({
         ingredient: item[`strIngredient${i}`],
         quantity: item[`strMeasure${i}`],

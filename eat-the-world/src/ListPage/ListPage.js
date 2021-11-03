1b7332e22e91ae20ca57005eaa201b2de3b9ccaa
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-03 10:00:15 +0100

    Fix ListPage style

--- eat-the-world/src/ListPage/ListPage.js
+++ eat-the-world/src/ListPage/ListPage.js
@@ -1,28 +1,19 @@
 import ListRecipe from 'common/ListRecipe/ListRecipe';
 import { useApiContext } from 'contexts/ApiContext';
-<<<<<<< HEAD
 import noResultsImg from 'assets/img/crying-orange.png';
+import './ListPage.scss';
 
 const ListPage = () => {
-  const { list } = useApiContext();
+  const { list, searchTerm } = useApiContext();
   console.log(list);
   return (
     <section className="list-page">
-      <p className="list-page__text">Search results for: Rice</p>
+      <p className="list-page__text">Search results for: {searchTerm}</p>
       {list ? (
         <ListRecipe list={list} />
       ) : (
         <img src={noResultsImg} alt="Orange emoji no results found" />
       )}
-=======
-import './ListPage.scss';
-const ListPage = () => {
-  const { list, searchTerm } = useApiContext();
-  return (
-    <section className="list-page">
-      <p className="list-page__text">Search results for: {searchTerm}</p>
-      <ListRecipe list={list} />
->>>>>>> main
     </section>
   );
 };

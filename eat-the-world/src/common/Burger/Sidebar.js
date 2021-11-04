7f93ee0cd765252363efdc455298fc00de453e4f
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-04 17:46:54 +0100

    Style sidebar

--- eat-the-world/src/common/Burger/Sidebar.js
+++ eat-the-world/src/common/Burger/Sidebar.js
@@ -1,7 +1,7 @@
 import { slide as Menu } from 'react-burger-menu';
-import forks from 'assets/icons/forks-white.svg';
 import star from 'assets/icons/start-white.svg';
 import home from 'assets/icons/white-house.svg';
+import user from 'assets/icons/user-icon.svg';
 import { Link } from 'react-router-dom';
 import './Sidebar.scss';
 
@@ -9,18 +9,17 @@ const Sidebar = (props) => {
   return (
     <Menu {...props}>
       <Link to="/">
-        <img src={home} alt="login" className="slide__icon" />
+        <img src={home} alt="home icon" className="slide__icon" />
         Home
       </Link>
-      <Link to="/recipes">
-        <img src={forks} alt="login" className="slide__icon" />
-        Recipes
-      </Link>
       <Link to="/favorites">
-        <img src={star} alt="login" className="slide__icon" />
+        <img src={star} alt="favorites icon" className="slide__icon" />
         Favorites
       </Link>
-      <Link to="/profile">Profile</Link>
+      <Link to="/profile">
+        <img src={user} alt="user icon" className="slide__icon" />
+        Profile
+      </Link>
     </Menu>
   );
 };

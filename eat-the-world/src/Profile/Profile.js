5330f901412ed2e774cb320f2bc2c1e8a887ad5b
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-04 16:45:41 +0100

    Add profile page

--- eat-the-world/src/Profile/Profile.js
+++ eat-the-world/src/Profile/Profile.js
@@ -1,7 +1,8 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import './Profile.scss';
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading, logout } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -9,11 +10,17 @@ const Profile = () => {
   console.log(user);
   return (
     isAuthenticated && (
-      <div>
+      <section className="profile">
         <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
+        <h2 className="profile__name">{user.name}</h2>
         <p>{user.email}</p>
-      </div>
+        <button
+          className="profile__logout-btn"
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Logout
+        </button>
+      </section>
     )
   );
 };

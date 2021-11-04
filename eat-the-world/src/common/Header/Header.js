d008bc2d721070b0b468ac886e28a505738f480c
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-04 15:38:41 +0100

    Add logout button

--- eat-the-world/src/common/Header/Header.js
+++ eat-the-world/src/common/Header/Header.js
@@ -1,4 +1,5 @@
 import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
 import './Header.scss';
 
 const Header = () => {
@@ -7,6 +8,7 @@ const Header = () => {
       <h1 className="header__title">Eat the World</h1>
       <div className="header__hamburger-login">
         <LoginButton />
+        <LogoutButton />
       </div>
     </header>
   );

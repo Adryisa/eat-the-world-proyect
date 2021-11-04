31802d003e667ab4ebbea54db48a5887ca5c089f
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-04 15:34:55 +0100

    Add login button

--- eat-the-world/src/common/Header/Header.js
+++ eat-the-world/src/common/Header/Header.js
@@ -1,12 +1,12 @@
+import LoginButton from './LoginButton';
 import './Header.scss';
-import loginIcon from '../../assets/icons/login.svg';
 
 const Header = () => {
   return (
     <header className="header">
       <h1 className="header__title">Eat the World</h1>
       <div className="header__hamburger-login">
-        <img src={loginIcon} alt="login" className="header__login-img" />
+        <LoginButton />
       </div>
     </header>
   );
--- eat-the-world/src/index.js
+++ eat-the-world/src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ApiContextProvider } from 'contexts/ApiContext';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <ApiContextProvider>
-        <App />
-      </ApiContextProvider>
+      <Auth0Provider
+        domain="dev-h4a2vu3r.us.auth0.com"
+        clientId="tjghdmpd1RWDfx8EOTJfKbBWN3AbPzmu"
+        redirectUri={window.location.origin}
+      >
+        <ApiContextProvider>
+          <App />
+        </ApiContextProvider>
+      </Auth0Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')

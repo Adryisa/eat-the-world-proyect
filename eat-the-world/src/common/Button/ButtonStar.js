4f9b9c9846dea665a14ecf27a29852b90697a80d
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-05 01:56:49 +0100

    Remove star button when user is not logged in

--- eat-the-world/src/common/Button/ButtonStar.js
+++ eat-the-world/src/common/Button/ButtonStar.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { useApiContext } from 'contexts/ApiContext';
 import starIcon from 'assets/icons/star.svg';
 import starIconFavorite from 'assets/icons/star-favorite.svg';
 import { addFavorites } from 'services/userServices';
 
 const ButtonStar = ({ item, type = 'api', className }) => {
+  const { isAuthenticated } = useAuth0();
   const [favorite, setFavorite] = useState(item.isFavorite);
   const { deleteOneRecipeNoDispatch, deleteOneRecipe } = useApiContext();
 
@@ -18,12 +20,14 @@ const ButtonStar = ({ item, type = 'api', className }) => {
   };
 
   return (
-    <button className={className} onClick={handleClick}>
-      <img
-        src={favorite ? starIconFavorite : starIcon}
-        alt={favorite ? 'favorite star icon' : 'favorite star icon selected'}
-      />
-    </button>
+    isAuthenticated && (
+      <button className={className} onClick={handleClick}>
+        <img
+          src={favorite ? starIconFavorite : starIcon}
+          alt={favorite ? 'favorite star icon' : 'favorite star icon selected'}
+        />
+      </button>
+    )
   );
 };
 

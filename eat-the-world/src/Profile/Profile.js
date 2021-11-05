cfadaaacd839af452d42576d2c3e410c29d3640b
Álvaro Serrano <69814675+alsersan@users.noreply.github.com>
2021-11-05 01:22:57 +0100

    Handle profile when user not logged in

--- eat-the-world/src/Profile/Profile.js
+++ eat-the-world/src/Profile/Profile.js
@@ -2,25 +2,37 @@ import { useAuth0 } from '@auth0/auth0-react';
 import './Profile.scss';
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading, logout } = useAuth0();
+  const { user, isAuthenticated, isLoading, logout, loginWithRedirect } =
+    useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
   }
   return (
-    isAuthenticated && (
-      <section className="profile">
-        <img src={user.picture} alt={user.name} />
-        <h2 className="profile__name">{user.name}</h2>
-        <p>{user.email}</p>
-        <button
-          className="profile__logout-btn"
-          onClick={() => logout({ returnTo: window.location.origin })}
-        >
-          Logout
-        </button>
-      </section>
-    )
+    <section className="profile">
+      {isAuthenticated ? (
+        <>
+          <img src={user.picture} alt={user.name} />
+          <h2 className="profile__name">{user.name}</h2>
+          <p>{user.email}</p>
+          <button
+            className="profile__btn"
+            onClick={() => logout({ returnTo: window.location.origin })}
+          >
+            Log out
+          </button>
+        </>
+      ) : (
+        <>
+          <p className="profile__not-logged">
+            You are not logged in. Please log in and try again.
+          </p>
+          <button className="profile__btn" onClick={() => loginWithRedirect()}>
+            Log in
+          </button>
+        </>
+      )}
+    </section>
   );
 };
 
